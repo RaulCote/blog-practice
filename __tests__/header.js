6e7af7af9c26b5f309133a306ee48db7c71b5cf1
RaulCote <raulcote@hotmail.com>
2020-01-22 19:44:20 +0100

    Added & set up React Testing Library. Already working.

--- __tests__/header.js
+++ __tests__/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Header from '../src/components/header';
+import { render } from '@testing-library/react';
 
 describe('Header', () => {
   it('renders correctly', () => {
@@ -8,5 +9,17 @@ describe('Header', () => {
       .create(<Header siteTitle="Default Starter" />)
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+});
+
+const Title = () => <h1 data-testid="hero-title">React Testing Library</h1>;
+
+describe('React Test Library', () => {
+  it('works as expected', () => {
+    const { getByTestId } = render(<Title />);
+
+    expect(getByTestId('hero-title')).toHaveTextContent(
+      'React Testing Library'
+    );
   });
 });
