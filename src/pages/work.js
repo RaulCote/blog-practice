aca80f70c9201f0c9822fa63c2252206f0ae87c4
Raul <raulcote@hotmail.com>
2020-07-04 17:52:37 +0200

    TODO comments on Work

--- src/pages/work.js
+++ src/pages/work.js
@@ -4,6 +4,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+// TODO : Work page
 const WorkPage = ({}) => {
   const data = useStaticQuery(graphql`
     query {
