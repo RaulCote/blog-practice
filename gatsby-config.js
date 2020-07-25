5934e1c9e1c1af8df85ed5bb5b2019ce10b564df
Raul <raulcote@hotmail.com>
2020-07-25 19:31:43 +0200

    Added reading time feature to blog posts, prismjs theme directly moved to appcontainer styles.

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [`gatsby-remark-prismjs`],
+        plugins: [`gatsby-remark-reading-time`, `gatsby-remark-prismjs`],
       },
     },
     `gatsby-transformer-sharp`,
