8c94f01370ef899206318756b0ef52c83988d426
Raul <raulcote@hotmail.com>
2020-08-01 13:36:27 +0200

    Added tests on layout for light / dark mode toggle.

--- __tests__/components/layout.spec.js
+++ __tests__/components/layout.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Layout from '../../src/components/layout';
 import * as Gatsby from 'gatsby';
 
@@ -26,7 +26,7 @@ describe('<Layout /> component tests', () => {
     );
 
     expect(getByTestId('layout-test')).toHaveTextContent('Welcome');
-    expect(getByText('Testing Layout')).toHaveTextContent('Testing Layout');
+    expect(getByText('Testing Layout')).toBeInTheDocument();
   });
 
   it('renders the NavBar content', () => {
@@ -45,5 +45,33 @@ describe('<Layout /> component tests', () => {
       </Layout>
     );
     expect(getByText('Gatsby')).toHaveTextContent('Gatsby');
+  });
+
+  it('can toggle from light to dark mode', async () => {
+    const { findByText, getByText } = render(
+      <Layout>
+        <h1 data-testid={'layout-test'}>Welcome</h1>
+      </Layout>
+    );
+
+    fireEvent.click(getByText('Dark'));
+    expect(await findByText('Light')).toBeInTheDocument();
+    fireEvent.click(getByText('Light'));
+    expect(await findByText('Dark')).toBeInTheDocument();
+  });
+
+  it(`will show directly on dark mode if user set it previously
+    in another visit`, async () => {
+    window.localStorage.setItem('isDarkMode', true);
+
+    const { findByText } = render(
+      <Layout>
+        <h1 data-testid={'layout-test'}>Welcome</h1>
+      </Layout>
+    );
+
+    expect(await findByText('Light')).toBeInTheDocument();
+
+    window.localStorage.removeItem('isDarkMode');
   });
 });
