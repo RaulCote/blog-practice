50eb2fd0fef55fa2849cf2134f662ce5f8a82326
Raul <raulcote@hotmail.com>
2020-08-05 18:37:09 +0200

    GlobalStyle back again.

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,8 @@ import { useStaticQuery, graphql } from 'gatsby';
 import { ThemeProvider } from 'styled-components';
 import lightTheme from '../themes/light';
 import darkTheme from '../themes/dark';
-// import { GlobalStyle } from '../ui/body';
-import { AppContainer } from '../ui/app-container';
+import { GlobalStyle } from '../ui/body';
+// import { AppContainer } from '../ui/app-container';
 import NavBar from './NavBar';
 import './layout.css';
 
@@ -44,29 +44,27 @@ const Layout = ({ children }) => {
   return (
     <>
       <ThemeProvider theme={currentTheme}>
-        {/* <GlobalStyle /> */}
-        <AppContainer>
-          <NavBar
-            siteTitle={data.site.siteMetadata.title}
-            toggleMode={toggleMode}
-            isDarkMode={isDarkMode}
-          />
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0,
-            }}
-          >
-            <main>{children}</main>
-            <footer>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </footer>
-          </div>
-        </AppContainer>
+        <GlobalStyle />
+        <NavBar
+          siteTitle={data.site.siteMetadata.title}
+          toggleMode={toggleMode}
+          isDarkMode={isDarkMode}
+        />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
       </ThemeProvider>
     </>
   );
