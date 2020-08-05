999ebbad61635a9e4b7ebc60c87d8b5a8eef3d80
Raul <raulcote@hotmail.com>
2020-08-05 18:32:17 +0200

    Getting rid of GlobalStyle, AppContainer works better.

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,8 @@ import { useStaticQuery, graphql } from 'gatsby';
 import { ThemeProvider } from 'styled-components';
 import lightTheme from '../themes/light';
 import darkTheme from '../themes/dark';
-import { GlobalStyle } from '../ui/body';
-
+// import { GlobalStyle } from '../ui/body';
+import { AppContainer } from '../ui/app-container';
 import NavBar from './NavBar';
 import './layout.css';
 
@@ -44,27 +44,29 @@ const Layout = ({ children }) => {
   return (
     <>
       <ThemeProvider theme={currentTheme}>
-        <GlobalStyle />
-        <NavBar
-          siteTitle={data.site.siteMetadata.title}
-          toggleMode={toggleMode}
-          isDarkMode={isDarkMode}
-        />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        {/* <GlobalStyle /> */}
+        <AppContainer>
+          <NavBar
+            siteTitle={data.site.siteMetadata.title}
+            toggleMode={toggleMode}
+            isDarkMode={isDarkMode}
+          />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </AppContainer>
       </ThemeProvider>
     </>
   );
