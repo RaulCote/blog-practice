7d84c3fbc3c219381750e2893f96bec30da838be
Raul <raulcote@hotmail.com>
2020-08-06 08:17:39 +0200

    blog-post . access object directly, no destructure.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -32,7 +32,7 @@ const BlogPostTemplate = ({ data, pageContext }) => {
         <h1>{title}</h1>
         <date>{date}</date>
         <h5>{displayReadingTime}</h5>
-        <p dangerouslySetInnerHTML={{ __html: html }} />
+        <p dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
       </article>
 
       <nav>
