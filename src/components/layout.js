da51fe47560ddbb3c35c1241990706206fb9adf2
Raul <raulcote@hotmail.com>
2020-09-19 13:51:45 +0200

    GlobalStyle back.

--- src/components/layout.js
+++ src/components/layout.js
@@ -76,29 +76,27 @@ const Layout = ({ children }) => {
   return (
     <>
       <ThemeProvider theme={currentTheme}>
-        {/* <GlobalStyle /> */}
-        <AppContainer>
-          <NavBar
-            siteTitle={data.site.siteMetadata.title}
-            toggleMode={toggleMode}
-            isDarkMode={isDarkMode}
-          />
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0,
-            }}
-          >
-            <main>{children}</main>
-            <footer>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </footer>
-          </div>
-        </AppContainer>
+        <GlobalStyle />
+        <NavBar
+          siteTitle={data.site.siteMetadata.title}
+          toggleMode={toggleMode}
+          isDarkMode={isDarkMode}
+        />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
       </ThemeProvider>
     </>
   );
