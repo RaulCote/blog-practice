00b1cec82316a18b883c550f78d548b684bb3723
Raul <raulcote@hotmail.com>
2020-09-19 13:24:36 +0200

    AppContainer back.

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'styled-components';
 import lightTheme from '../themes/light';
 import darkTheme from '../themes/dark';
 import { GlobalStyle } from '../ui/body';
-// import { AppContainer } from '../ui/app-container';
+import { AppContainer } from '../ui/app-container';
 import NavBar from './NavBar';
 import './layout.css';
 
@@ -76,27 +76,29 @@ const Layout = ({ children }) => {
   return (
     <>
       <ThemeProvider theme={currentTheme}>
-        <GlobalStyle />
-        <NavBar
-          siteTitle={data.site.siteMetadata.title}
-          toggleMode={toggleMode}
-          isDarkMode={isDarkMode}
-        />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        {/* <GlobalStyle /> */}
+        <AppContainer>
+          <NavBar
+            siteTitle={data.site.siteMetadata.title}
+            toggleMode={toggleMode}
+            isDarkMode={isDarkMode}
+          />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </AppContainer>
       </ThemeProvider>
     </>
   );
