78d94b2e22ed2650d93042545723bca1cc7278a3
Raul <raulcote@hotmail.com>
2020-10-17 18:03:27 +0200

    Changed mobile limit size, tablets included.

--- src/ui/variables.js
+++ src/ui/variables.js
@@ -1,7 +1,7 @@
 export const MARGIN_CONTAINER = 'auto';
 export const MAX_WIDTH_CONTAINER = '960px';
-export const MEDIA_QUERY_MOBILE_FROM = '@media screen and (min-width: 601px)';
-export const MEDIA_QUERY_MOBILE_LIMIT = '@media screen and (max-width: 600px)';
+export const MEDIA_QUERY_MOBILE_FROM = '@media screen and (min-width: 750px)';
+export const MEDIA_QUERY_MOBILE_LIMIT = '@media screen and (max-width: 750px)';
 export const MEDIA_QUERY_REGULAR_SCREEN_FROM =
   '@media screen and (min-width: 1001px)';
 export const MEDIA_QUERY_REGULAR_SCREEN_LIMIT =
